6df6da1c7d2cb7606e38228f222081fe4b2ece79
brgcode <van.mele@arch.ethz.ch>
2021-12-16 22:52:05 +0100

    basic version

---
+++ doc.rst
@@ -0,0 +1,13 @@
+**********************************
+Transporting a Box Along a Curve
+**********************************
+
+.. figure:: example-transporting-a-box-along-a-curve.png
+    :figclass: figure
+    :class: figure-img img-fluid
+
+.. literalinclude:: environment.yml
+    :language: toml
+
+.. literalinclude:: script.py
+    :language: python
---
+++ script.py
@@ -0,0 +1,28 @@
+from compas.geometry import Point, Box, Transformation
+from compas.geometry import Polyline, NurbsCurve
+from compas_view2.app import App
+
+curve = NurbsCurve.from_points([Point(0, 0, 0), Point(3, 6, 0), Point(6, -3, 3), Point(10, 0, 0)])
+frames = [curve.frame_at(t) for t in curve.space(50)]
+xforms = [Transformation.from_frame_to_frame(frames[0], frame) for frame in frames]
+
+box = Box(frames[0], 2, 1, 0.5)
+
+viewer = App(width=1600, height=900)
+viewer.view.camera.rz = 90
+viewer.view.camera.rx = -75
+viewer.view.camera.tx = 0
+viewer.view.camera.ty = -1
+viewer.view.camera.distance = 6
+
+viewer.add(Polyline(curve.locus()))
+viewer.add(box.frame)
+boxobj = viewer.add(box, opacity=0.5)
+
+@viewer.on(interval=100, frames=len(frames))
+def move(f):
+   viewer.add(frames[f])
+   boxobj.matrix = xforms[f].matrix
+   boxobj.update()
+
+viewer.show()
